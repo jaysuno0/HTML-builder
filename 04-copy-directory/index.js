9aafb02a9b149d86cf3ac97c44440c31cfa05a0e
Daniil Kochkin <jaysun0@yandex.ru>
2022-05-22 07:40:58 +0300

    feat: solve the fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
+const path = require('path');
+const fs = require('fs/promises');
+const filesPath = path.join(__dirname, 'files');
+const copyFilesPath = path.join(__dirname, 'copy-files');
 
+function copyDir(fromPath, toPath) {
+  fs.mkdir(toPath, { recursive: true });
+  fs.readdir(fromPath)
+    .then((data) => {
+      data.forEach(file => {
+        fs.copyFile(path.join(fromPath, file), path.join(toPath, file));
+      });
+    })
+    .catch((err) => {
+      throw err;
+    });
+}
+
+copyDir(filesPath, copyFilesPath);
