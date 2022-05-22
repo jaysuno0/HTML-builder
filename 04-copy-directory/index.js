dddd8de73f36b21771947a66dc89bf3d9d1a3e40
Daniil Kochkin <jaysun0@yanex.ru>
2022-05-22 21:57:11 +0300

    feat: add functional to copdir function
    now "copyDir" function can copy not only files but also folders with files inside recursively

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,6 +3,10 @@ const fs = require('fs/promises');
 const filesPath = path.join(__dirname, 'files');
 const copyFilesPath = path.join(__dirname, 'copy-files');
 
+process.on('exit', () => {
+  process.stdout.write('created a full copy');
+});
+
 function copyDir(fromPath, toPath) {
   fs.rm(toPath, { force: true, recursive: true })
     .then(() => {
@@ -10,15 +14,17 @@ function copyDir(fromPath, toPath) {
       fs.readdir(fromPath)
         .then((data) => {
           data.forEach(file => {
-            fs.copyFile(path.join(fromPath, file), path.join(toPath, file));
+            if (path.extname(file) === '') {
+              copyDir(path.join(fromPath, file), path.join(toPath, file));
+            } else {
+              fs.copyFile(path.join(fromPath, file), path.join(toPath, file));
+            }
           });
         });
     })
     .catch((err) => {
       throw err;
     });
-
-  process.stdout.write('created a full copy');
 }
 
 copyDir(filesPath, copyFilesPath);
