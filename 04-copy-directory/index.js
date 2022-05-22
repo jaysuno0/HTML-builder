8434d520d077216081209aa1fe40b8092b04a4ae
Daniil Kochkin <jaysun0@yandex.ru>
2022-05-22 08:11:55 +0300

    feat: minor changes according to 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,16 +4,21 @@ const filesPath = path.join(__dirname, 'files');
 const copyFilesPath = path.join(__dirname, 'copy-files');
 
 function copyDir(fromPath, toPath) {
-  fs.mkdir(toPath, { recursive: true });
-  fs.readdir(fromPath)
-    .then((data) => {
-      data.forEach(file => {
-        fs.copyFile(path.join(fromPath, file), path.join(toPath, file));
-      });
+  fs.rm(toPath, { force: true, recursive: true })
+    .then(() => {
+      fs.mkdir(toPath);
+      fs.readdir(fromPath)
+        .then((data) => {
+          data.forEach(file => {
+            fs.copyFile(path.join(fromPath, file), path.join(toPath, file));
+          });
+        });
     })
     .catch((err) => {
       throw err;
     });
+
+  process.stdout.write('created a full copy');
 }
 
 copyDir(filesPath, copyFilesPath);
