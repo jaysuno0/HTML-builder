3ee76ca505586d2e2495da999f2462084018f391
Daniil Kochkin <jaysun0@yanex.ru>
2022-05-22 14:22:41 +0300

    feat: solve fifth task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const fsPromises = require('fs/promises');
+const fs = require('fs');
+const stdout = process.stdout;
+const stylesFolderPath = path.join(__dirname, 'styles');
+const bundlePath = path.join(__dirname, 'project-dist', 'bundle.css');
+
+process.on('exit', () => {
+  stdout.write('css build is ready');
+});
+
+fsPromises.writeFile(bundlePath, '');
+
+fsPromises.readdir(stylesFolderPath)
+  .then((files) => {
+    let count = -1;
+
+    (function streamToBundle(){
+      count++;
+      if (count < files.length && path.extname(files[count]) === '.css') {
+        const readStream = fs.createReadStream(path.join(stylesFolderPath, files[count]));
+
+        readStream.on('data', (chunk) => {
+          fs.appendFile(bundlePath, chunk, (err) => {
+            if (err) throw err;
+          });
+        });
+
+        readStream.on('close', streamToBundle);
+      }
+    })();
+  })
+  .catch((err) => {
+    throw err;
+  });
+
